9a36b1ef9d39e38a0664e62bc6739ba04683251c
luiz oberto <lom.raiol@gmail.com>
2025-06-01 12:39:54 -0300

    update testes.py - AI feedback

--- testes.py
+++ testes.py
@@ -1,7 +1,7 @@
 import random
 
 marcadores = ["O", "X"]
-teste = [
+tabuleiro = [
         ["1", "2", "3"],
         ["4", "5", "6"],
         ["7", "8", "9"]
@@ -10,6 +10,15 @@ teste = [
 # teste do tabuleiro
 def random_insert(marcador):
     # seleciona um elemento aleatório
+    while True:
+        linha = random.randint(0, 2)
+        coluna = random.randint(0, 2)
+
+        if tabuleiro[linha][coluna] not in ["O", "X"]:
+            tabuleiro[linha][coluna] = marcador
+            return
+    
+    '''
     escolher_linha = random.choice(teste)
     print(escolher_linha)
     escolher_elemento = random.choice(escolher_linha)
@@ -35,12 +44,20 @@ def random_insert(marcador):
                 coluna += 1
 
             linha += 1
+    '''
 
 
-
-def verifica_linha(tabuleiro):
+# === VERIFICAÇÕES DE LINHAS, COLUNAS E DIAGONAIS ===
+# FALTA VERIFICAR DIAGONAIS!!!
+def verifica_linha(tab):
     ''' 
     verifica se uma linha possui 3 elementos iguais
+    '''
+    for linha in tab:
+        if linha[0] == linha[1] == linha[2]:
+            return True
+    return False
+
     '''
     game_over = False
 
@@ -63,12 +80,18 @@ def verifica_linha(tabuleiro):
             break
             
     return game_over
-
+    '''
 
     
-def verifica_coluna():
+def verifica_coluna(tab):
     '''
     Verifica se uma coluna possui os três elementos iguais
+    '''
+    for i in range(3):
+        if tab[0][i] == tab[1][i] == tab[2][i]:
+            return True
+    return False
+
     '''
     i = 0
     coluna_linha = []
@@ -76,40 +99,36 @@ def verifica_coluna():
     while i <= 2:
         colunas = []
         # transforma as colunas em linhas
-        for linha in teste:
+        for linha in tabuleiro:
             colunas.append(linha[i])
             
         coluna_linha.append(colunas)
         i+=1
     
     return verifica_linha(coluna_linha)
+    '''
 
-#################################################################
-i = 0
-selecao = 0
-while i < 9:
-    objeto = marcadores[selecao]
-    random_insert(objeto)
+def main():
+    turno = 0
+    while turno < 9:
+        jogador = marcadores[turno % 2]
+        random_insert(jogador)
 
-    # verificar se game_over
-    if verifica_linha(teste):
-        print("3 elementos iguais numa linha!")
-        print("O jogo acabou")
-        break
-    
-    elif verifica_coluna():
-        print("3 elementos iguais numa coluna!")
-        print("O jogo acabou")
-        break
-    
-
-    if selecao == 0:
-        selecao = 1
-    elif selecao == 1:
-        selecao = 0
-
-    i+=1    
+        # verificar se game_over
+        if verifica_linha(tabuleiro):
+            print("3 elementos iguais numa linha!")
+            break
+        
+        elif verifica_coluna(tabuleiro):
+            print("3 elementos iguais numa coluna!")
+            break
 
 
-for linha in teste:
-    print(linha)
+        turno+=1    
+
+
+    for linha in tabuleiro:
+        print(linha)
+
+
+main()
